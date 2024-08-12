9e8ec6f11fd8f40803030499ce647465cd0e37b5
Renuramchandra <renuramchandra649@gmail.com>
2024-08-12 12:09:16 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const app = express()
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // mongodb connection //
 
@@ -35,6 +35,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  app.use('/home', (req,res) =>{
-    res.send("Welcome to Mern project")
+  app.use('/', (req,res) =>{
+    res.send("Welcome to Mern project");
   })
