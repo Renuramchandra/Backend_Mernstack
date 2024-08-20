0dbfec8f21ddf4eb0bbbedd729db725e9af404ed
Renuka k <164310372+Renuramchandra@users.noreply.github.com>
2024-08-20 19:43:17 +0530

    Update firmController.js

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -38,12 +38,12 @@ const addFirm = async(req, res)=>{
         })
     
          const savedFirm  =   await firm.save();
-
+          const firmId = savedFirm._id
          user.firm.push(savedFirm)
 
          await user.save()
 
-        return res.status(200).json({message: 'Firm Added Sucessfully' })
+        return res.status(200).json({message: 'Firm Added Sucessfully', firmId })
     }catch(error) {
         console.error(error)
         res.status(500).json("Internal server error")
